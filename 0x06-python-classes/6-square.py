8a7abba8e917855693d85608ba59f0370fad9bfe
rana <rana.alashry6@gmail.com>
2024-04-12 06:13:17 -0700

    7th

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -46,6 +46,7 @@ class Square:
         '''Prints the square.'''
         if (self.__size == 0):
             print()
+            return
         for j in range(self.__position[1]):
             print()
         for i in range(self.__size):
