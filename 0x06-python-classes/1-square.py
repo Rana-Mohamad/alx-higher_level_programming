13314d212dde8eabd738455e3409750bdfc444e2
rana <rana.alashry6@gmail.com>
2024-04-12 02:37:09 -0700

    2nd

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,5 +4,11 @@
 
 class Square:
     '''Define a square.'''
+
     def __init__(self, size):
+        '''Constructor.
+
+        Args:
+            size: length of the side of the square.
+        '''
         self.__size = size
