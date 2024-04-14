755276b6a096cada0083c1792784e02729996988
rana <rana.alashry6@gmail.com>
2024-04-14 03:00:48 -0700

    1st

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''Add two integers module.'''
+
+
+def add_integer(a, b=98):
+    '''Add two integers.
+
+    Args:
+        a: the first integer/float number.
+        b: the second integer/float number, it takes  98 as default value.
+
+    Raises:
+        TypeError: if a, b not integer or float.
+
+    Returns:
+        The integer sum of the two values.
+    '''
+
+    if (type(a)) not in (int, float):
+        raise TypeError("a must be an integer")
+    if (type(b)) not in (int, float):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
