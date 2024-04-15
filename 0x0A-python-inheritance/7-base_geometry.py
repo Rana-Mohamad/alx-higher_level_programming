174dd6522914318ca80741f69ee2b3f96dcc8df8
rana <rana.alashry6@gmail.com>
2024-04-15 15:10:51 -0700

    8th

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''BaseGeometry module.'''
+
+
+class BaseGeometry:
+    '''BaseGeometry class.'''
+
+    def area(self):
+        '''Method to compute the area.'''
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        '''Integer validation method.
+
+        Args:
+            name: always a string.
+            value: an integer number.
+        '''
+
+        if (type(value) != int):
+            raise TypeError("{} must be an integer".format(name))
+        if (value <= 0):
+            raise ValueError("{} must be greater than 0".format(name))
