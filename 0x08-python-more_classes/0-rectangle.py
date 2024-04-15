ed3cdc7d1131690fff7c14de008ea78a87738075
rana <rana.alashry6@gmail.com>
2024-04-15 04:06:12 -0700

    1st

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Rectangle module.'''
+
+
+class Rectangle:
+    '''Defines a rectangle.'''
+
+    @classmethod
+    def rectangle(cls, Rectangle):
+        return cls, Rectangle
