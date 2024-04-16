8a01f25069b23d04ff13296e6ebe17feb3cb9a6b
rana <rana.alashry6@gmail.com>
2024-04-16 06:56:00 -0700

    4th

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''To json string module.'''
+
+
+import json
+
+
+def to_json_string(my_obj):
+    '''Returns the json representation of an object.'''
+
+    return json.dumps(my_obj)
