df4cadcacc5c4f8aeca5e902706047db0050fdaf
rana <rana.alashry6@gmail.com>
2024-04-17 02:54:22 -0700

    12th

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''Student module.'''
+
+
+class Student:
+    '''Student class.'''
+
+    def __init__(self, first_name, last_name, age):
+        '''Constructor.
+
+        Args:
+            first_name: the first name of the student.
+            last_name: the last name of the student.
+            age: the student age.
+        '''
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''Retrieves a dictionary representation.
+        Args:
+            attrs: the attributes to represent.
+        '''
+
+        if (type(attrs) is list and all(type(elm) is str for elm in attrs)):
+            return {key: getattr(self, key)
+                    for key in attrs if hasattr(self, key)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        '''Replaces all attributes of the Student.'''
+
+        for key, value if json.items():
+            setattr(self, key, value)
