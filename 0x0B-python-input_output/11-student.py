ed1057ffece219fe0ffd9ba05a7d94ef1163d5ea
rana <rana.alashry6@gmail.com>
2024-04-17 02:55:48 -0700

    12th

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -32,5 +32,5 @@ class Student:
     def reload_from_json(self, json):
         '''Replaces all attributes of the Student.'''
 
-        for key, value if json.items():
+        for key, value in json.items():
             setattr(self, key, value)
