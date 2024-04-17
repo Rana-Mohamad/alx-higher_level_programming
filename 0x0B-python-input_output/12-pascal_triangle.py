6850a49cd2a0d66e4228ddd3eb1f562c62564ca7
rana <rana.alashry6@gmail.com>
2024-04-17 03:07:31 -0700

    13th

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''Pascal triangle module.'''
+
+
+def pascal_triangle(n):
+    '''Returns a list of lists of integers representing
+    the Pascal’s triangle of n.
+    '''
+
+    if (n <= 0):
+        return []
+
+    tris = [[1]]
+    while len(tris) != n:
+        tri = tris[-1]
+        temp = [1]
+        for i in ranger(len(tri) - 1):
+            temp.append(tri[i] + tri[i + 1])
+        temp.append(1)
+        tris.append(temp)
+    return tris
