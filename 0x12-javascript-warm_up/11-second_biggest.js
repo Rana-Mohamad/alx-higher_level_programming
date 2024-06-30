fa0785dc42931972b15738de98c51098921bf199
rana <rana.alashry6@gmail.com>
2024-06-30 00:58:25 -0700

    12th

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+if (!process.argv[2] || !process.argv[3]) {
+  console.log(0);
+} else {
+  let biggest = parseInt(process.argv[2]);
+  let secBig = parseInt(process.argv[2]);
+  for (let i = 2; i < process.argv.length; i++) {
+    if (parseInt(process.argv[i]) > biggest) {
+      biggest = parseInt(process.argv[i]);
+    }
+  }
+  for (let j = 2; j < process.argv.length; j++) {
+    if (parseInt(process.argv[j]) < biggest && parseInt(process.argv[j]) > secBig) {
+      secBig = parseInt(process.argv[j]);
+    }
+  }
+  console.log(secBig);
+}
