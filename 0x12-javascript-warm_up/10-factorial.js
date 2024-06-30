34ba41b41423213372572353c58ae97dbc5e04fe
rana <rana.alashry6@gmail.com>
2024-06-30 00:23:54 -0700

    11th

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+function factorial (a) {
+  if (!Number(a) || a <= 1) {
+    return 1;
+  } else {
+    return a * factorial(a - 1);
+  }
+}
+
+console.log(factorial(parseInt(process.argv[2])));
