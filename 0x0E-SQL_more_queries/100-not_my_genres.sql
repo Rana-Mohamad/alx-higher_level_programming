c89d0c82111b14356db6e8d7aa61e23bc524e7ef
Rana <rana.alashry6@gmail.com>
2024-09-18 04:26:19 -0700

    add alias

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -2,20 +2,21 @@
 -- Uses the hbtn_0d_tvshows database
 -- Results sorted in ascending order by the genre name
 SELECT DISTINCT `name`
-	FROM `tv_genres`
-		INNER JOIN `tv_show_genres`
-		ON `tv_genres.id` = `tv_show_genres.genre_id`
+	FROM `tv_genres` AS g
+		INNER JOIN `tv_show_genres` AS s
+		ON g.`id` = s.`genre_id`
 
-		INNER JOIN `tv_shows`
-		ON `tv_show_genres.show_id` = `tv_shows.id`
-		WHERE `tv_genres.name` NOT IN(
+		INNER JOIN `tv_shows` AS t
+		ON s.`show_id` = t.`id`
+		WHERE g.`name` NOT IN(
 			SELECT `name`
-				FROM `tv_genres`
-					INNER JOIN `tv_show_genres`
-					ON `tv_genres.id` = `tv_show_genres.genre_id`
+				FROM `tv_genres` AS g
+					INNER JOIN `tv_show_genres` AS s
+					ON g.`id` = s.`genre_id`
 
-					INNER JOIN `tv_shows`
-					ON `tv_show_genres.show_id` = `tv_shows.id`
-					WHERE `tv_shows.title` = "Dexter")
-ORDER BY `tv_genres.name`;
+					INNER JOIN `tv_shows` AS t
+					ON s.`show_id` = t.`id`
+					WHERE t.`title` = "Dexter"
+				)
+	ORDER BY g.`name`;
 
