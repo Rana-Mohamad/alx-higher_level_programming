02f51741bcdc0087ef1163f6183ef27be4575aac
Rana <rana.alashry6@gmail.com>
2024-09-23 05:06:12 -0700

    filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     cur.execute("SELECT * FROM states ORDER BY states.id")
     rows = cur.fetchall()
     for row in rows:
-        if row[1][0] == 'N'
-        print(row)
+        if row[1][0] == 'N':
+            print(row)
     cur.close()
     db.close()
